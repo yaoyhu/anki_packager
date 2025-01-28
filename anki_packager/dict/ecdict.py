88c542f61aee423823dd38cda7076bdccaa10a3e
Yaoyao Hu <shady030314@gmail.com>
2025-01-28 10:21:13 +0800

    fix: typos in ecdict

--- anki_packager/dict/ecdict.py
+++ anki_packager/dict/ecdict.py
@@ -5,16 +5,21 @@ import sqlite3
 from anki_packager.dict.ECDICT import stardict
 
 
-class Ecdit:
+class Ecdict:
     def __init__(self):
         # ecdict path: ./ECDICT/
         self.ecdict_path = os.path.join(os.path.dirname(__file__), "ECDICT")
-        self.csv = os.path.join(self.ecdict_path, "startdict.csv")
-        self.sqlite = os.path.join(self.ecdict_path, "ecdict.db")
+        self.csv = os.path.join(self.ecdict_path, "stardict.csv")
+        self.sqlite = os.path.join(self.ecdict_path, "stardict.db")
         self._convert()
         self.conn = sqlite3.connect(self.sqlite)
         self.cursor = self.conn.cursor()
         self.sd = stardict.StarDict(self.sqlite, False)
+
+    def __del__(self):
+        if hasattr(self, "conn"):
+            self.cursor.close()
+            self.conn.close()
 
     def _convert(self):
         if not os.path.exists(self.sqlite):
@@ -22,3 +27,14 @@ class Ecdit:
 
     def ret_word(self, word):
         return self.sd.query(word)
+
+
+if __name__ == "__main__":
+    ecdict = Ecdict()
+    print(ecdict.ret_word("hello"))
+    print("")
+    print(ecdict.ret_word("world"))
+    print("")
+    print(ecdict.ret_word("python"))
+    print("")
+    print(ecdict.ret_word("dictionary"))
